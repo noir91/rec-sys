1b56af6d8ec3a5a4fecd39d3031816ed359e3552
noir91 <noorpro92@gmail.com>
2025-08-03 14:10:32 +0400

    Added functions to frontend for testing, improved code

--- streamlit_app/st_app.py
+++ streamlit_app/st_app.py
@@ -8,26 +8,26 @@ import streamlit as st
 import requests as r
 import json
 
-# Streamlit
+# Functions
+def fetch_movie_title():
+    res_data = r.get('http://backend:8000/getdata')
+    return res_data.json()['title']
+
+def get_response_data(): # frontend gets json
+    payload = {"movie_name": selected_movie_name, "top_n" : 6}
+    response = r.post( url = "http://backend:8000/recommend", json = payload)
+    return response.json()
+
+# -- STREAMLIT (UI) --
+
 st.title('Content-Based Movie Recommender system')
-
-res_data = r.get('http://backend:8000/getdata')
-movies_titles = res_data.json()['title']
-
-selected_movie_name = st.selectbox(
-    '',
-    movies_titles)
-#name = {'movie_name': selected_movie_name}
+titles = fetch_movie_title()
+selected_movie_name = st.selectbox('', titles)
 
 # - When user clicks on button, we will fetch it from the API
-if st.button('Recommend'):
-    # Front End sending json
-    payload = {"movie_name": selected_movie_name, "top_n" : 6}
-    response = r.post( url = "http://backend:8000/recommend",
-                    json = payload)
-    # Response json
-    data = response.json()
 
+data = get_response_data()
+if st.button('Recommend'):
     # titles and posters being extracted from RecommendationResponse made into json
     names = data['titles']
     posters = data['posters']
