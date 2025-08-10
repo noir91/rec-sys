8b8724111f0b64b55c7e4aa1c16db2810e6549ff
Noorullah Y <noorpro92@gmail.com>
2025-08-10 18:11:15 +0400

    attempt to fix unhealthy docker image in EC2 for streamlit (2)

--- streamlit_app/st_app.py
+++ streamlit_app/st_app.py
@@ -17,26 +17,26 @@ def post_response_data(title): # frontend gets json
     response = requests.post( url = "http://backend:8000/recommend", json = payload)
     return response.json()       
 
-def main():
-    # -- STREAMLIT (UI) --
 
-    st.title('Content-Based Movie Recommender system')
-    titles = fetch_movie_title()
-    selected_movie_name = st.selectbox('', titles)
+# -- STREAMLIT (UI) --
 
-    # - When user clicks on button, we will fetch it from the API
+st.title('Content-Based Movie Recommender system')
+titles = fetch_movie_title()
+selected_movie_name = st.selectbox('', titles)
 
-    data = post_response_data(selected_movie_name)
-    if st.button('Recommend'):
-        # titles and posters being extracted from RecommendationResponse made into json
-        names = data['titles']
-        posters = data['posters']
+# - When user clicks on button, we will fetch it from the API
 
-        cols = st.columns(5)
-        for i in range(5):
-            with cols[i]:
-                title = names[i]
-                st.subheader(names[i])
-                st.image(posters[i])
+data = post_response_data(selected_movie_name)
+if st.button('Recommend'):
+    # titles and posters being extracted from RecommendationResponse made into json
+    names = data['titles']
+    posters = data['posters']
+
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            title = names[i]
+            st.subheader(names[i])
+            st.image(posters[i])
 
     
