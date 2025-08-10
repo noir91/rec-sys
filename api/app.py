fe10e671964bf74fcf7bdffac784b6f0ac92e03a
Noorullah Y <noorpro92@gmail.com>
2025-08-10 19:17:22 +0400

    attempt to solve unhealthy docker image error for frontend - detailed description - the api/app.py had a asynccontextmanager decorated with a lifespan function for fastapi to cache the data once loaded, so it can be reused the response time can be increased but it was commented out, probably because during dev it was kept that way, it has now been resolved. this error cause backend:8000/getdata route not to return anything but Internal server error because it couldn't find the data.

--- api/app.py
+++ api/app.py
@@ -1,17 +1,23 @@
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import requests as r
 from api.routes import recommend
+from api.logic.load_data import data
+
+# caching the data on startup for 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    movies, sim_mat = data()
+    app.state.movies = movies
+    app.state.sim_mat = sim_mat
+    yield
 
 # Recommend and getdata routes
-app = FastAPI(title = 'Content Based Recommender System')
+app = FastAPI(title = 'Content Based Recommender System', lifespan= lifespan)
 app.include_router(recommend.apirouter)
 
 
-# caching the data on startup for streamlit
-#async def lifespan(app: FastAPI):
-    #movies, sim_mat = data()
-    #app.state.movies = movies
-    #app.state.sim_mat = sim_mat
-    #yield
+
 
 
